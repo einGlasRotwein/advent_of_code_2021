d4a9aad25ea76dd81652f60ea2bd437b7a6ef1d7
einGlasRotwein <juliane.tkotz@gmail.com>
2021-12-01 08:06:45 +0100

    completed day 1

---
+++ day01.R
@@ -0,0 +1,12 @@
+
+day1 <- as.numeric(readLines("input01.txt"))
+
+#### PART 1 ####
+# Number of times the measurement increased
+sum(diff(day1) > 0)
+
+#### PART 2 ####
+# Same as above, but this time, we don't compare individual measurements, but
+# use a sliding window to calculate the sum of three data points each.
+sliding_sums <- zoo::rollsum(day1, 3)
+sum(diff(sliding_sums) > 0)
