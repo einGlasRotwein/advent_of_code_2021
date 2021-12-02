2cc628b30fda275a0bf3a05683e96dda97be5753
einGlasRotwein <juliane.tkotz@gmail.com>
2021-12-02 09:39:55 +0100

    removed white border

--- day02.R
+++ day02.R
@@ -102,7 +102,7 @@ commands$cumsum[nrow(commands)] * commands$depth[nrow(commands)]
     labs(x = "horizontal") +
     theme_classic() +
     theme(
-      panel.background = element_rect(fill = NA, colour = "white", size = 1),
+      panel.background = element_rect(fill = NA, colour = NA, size = 1),
       panel.grid.major = element_line(colour = "#00ad2b"),
       plot.background = element_rect(fill = "#0c1017", colour = NA),
       strip.background = element_rect(fill = NA, colour = NA),
