899c81c749c19bd677af1c88c9f92bb449eac68f
einGlasRotwein <juliane.tkotz@gmail.com>
2021-12-05 10:26:39 +0100

    added vent plot to README

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Solving the [advent of code](https://adventofcode.com/) puzzles in R.
 Here is a little “radar plot” for the submarine on day 2.
 
 <img src="README_files/figure-gfm/unnamed-chunk-1-1.png" width="90%" />
+
+For day 5, I plotted the hydrothermal vents. The colour scheme was
+inspired by the first picture in the [Wikipedia
+article](https://en.wikipedia.org/wiki/Hydrothermal_vent#/media/File:Champagne_vent_white_smokers.jpg),
+and I used the package
+[ggecho](https://coolbutuseless.github.io/2019/03/22/ggecho-an-experimental-ggplot-stat-for-blurring-elements/)
+to blurr the lines of the plot, because I thought that this is what
+represents the smoke of the vents best.
+
+<img src="README_files/figure-gfm/unnamed-chunk-2-1.png" width="90%" />
---
+++ day05_plot_only.R
@@ -0,0 +1,48 @@
+
+# Only the hydrothermal vent plot I did for day 5, without the puzzle solution
+
+library(tidyverse)
+
+# For blurring the lines in the plot.
+# Get the package from here
+# remotes::install_github("coolbutuseless/ggecho")
+library(ggecho)
+
+day5 <- read.table("inputs/input05.txt")
+
+# Bring into dataframe with x1, y1, x2, y2 cols
+day5 <- 
+  day5 %>% 
+  select(-V2) %>% 
+  separate(V1, c("x1", "y1"), sep = ",") %>%
+  separate(V3, c("x2", "y2"), sep = ",")
+
+# Plot danger zones
+to_plot <- 
+  day5 %>% 
+  rownames_to_column("id") %>% 
+  pivot_longer(
+    x1:y2,
+    names_to = c("axis", "position"),
+    values_to = "value",
+    names_pattern = "(.)(.)"
+  ) %>% 
+  pivot_wider(names_from = "axis") %>% 
+  mutate(across(x:y, as.numeric))
+
+vent_plot <- 
+  to_plot %>% 
+  ggplot(aes(x = x, y = y, group = id)) +
+  geom_path(colour = "#00f0d5", alpha = .5, stat = "echo", alpha_factor = .3) +
+  geom_point(colour = "#00f0d5") +
+  theme_classic() +
+  
+  theme(
+    panel.background = element_rect(fill = NA, colour = NA, size = 1),
+    panel.grid.major = element_line(colour = "#005a5f"),
+    plot.background = element_rect(fill = "#00152e", colour = "white"),
+    axis.text = element_text(size = 12, colour = "white"),
+    axis.title = element_text(size = 14, colour = "white"),
+    axis.line = element_line(colour = "white", size = 1),
+    legend.position = "none"
+  ) 
