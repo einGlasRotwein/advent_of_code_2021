c94f86806f4b96a71fb7373c7677e78a0449db33
einGlasRotwein <juliane.tkotz@gmail.com>
2021-12-05 10:17:26 +0100

    added vent plot

--- day05.R
+++ day05.R
@@ -84,3 +84,39 @@ coordinate_table <-
   count()
 
 sum(table(coordinate_table$n)[-1])
+
+## BONUS ##
+# Get the package from here
+# remotes::install_github("coolbutuseless/ggecho")
+library(ggecho)
+
+# Plot danger zones
+to_plot <- 
+  day5 %>% 
+  rownames_to_column("id") %>% 
+  pivot_longer(
+    x1:y2,
+    names_to = c("axis", "position"),
+    values_to = "value",
+    names_pattern = "(.)(.)"
+  ) %>% 
+  pivot_wider(names_from = "axis") %>% 
+  mutate(across(x:y, as.numeric))
+
+vent_plot <- 
+  to_plot %>% 
+  ggplot(aes(x = x, y = y, group = id)) +
+  geom_path(colour = "#00f0d5", alpha = .5, stat = "echo", alpha_factor = .3) +
+  geom_point(colour = "#00f0d5") +
+  theme_classic() +
+  
+  theme(
+    panel.background = element_rect(fill = NA, colour = NA, size = 1),
+    panel.grid.major = element_line(colour = "#005a5f"),
+    plot.background = element_rect(fill = "#00152e", colour = "#00152e"),
+    axis.text = element_text(size = 12, colour = "white"),
+    axis.title = element_text(size = 14, colour = "white"),
+    axis.line = element_line(colour = "white", size = 1),
+    legend.position = "none"
+  ) 
+
